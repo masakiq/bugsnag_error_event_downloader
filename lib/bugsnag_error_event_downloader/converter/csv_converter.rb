0e4c7b00181a776ca02db8a76134b3fba8269533
Dai Okumura <dai.okumura@socialplus.jp>
2023-06-29 01:30:08 +0900

    Support for Sawyer::Resource on conversion of hash

--- lib/bugsnag_error_event_downloader/converter/csv_converter.rb
+++ lib/bugsnag_error_event_downloader/converter/csv_converter.rb
@@ -21,10 +21,10 @@ module BugsnagErrorEventDownloader
           headers = csv_map.map { |m| m["header"] }
           rows << headers
           events.each do |event|
+            json = deep_hash(event).to_json
             paths = csv_map.map { |m| m["path"] }
             row = paths.map do |path|
               json_path = JsonPath.new(path)
-              json = event.to_h.to_json
               begin
                 json_path.on(json).uniq.join(",")
               rescue ArgumentError
@@ -37,6 +37,21 @@ module BugsnagErrorEventDownloader
       end
 
       private
+
+      def deep_hash(object)
+        case object
+        when Hash
+          object.each_with_object({}) do |(key, value), hash|
+            hash[key] = deep_hash(value)
+          end
+        when Array
+          object.map { |value| deep_hash(value) }
+        when Sawyer::Resource
+          deep_hash(object.to_h)
+        else
+          object
+        end
+      end
 
       def parse_csv_map(path)
         csv_map_file = File.read(path)
